06870a7b2038ad4acd1e5044f002e8e6d783406f
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-10-29 10:57:46 +0100

    exo4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $(function() {
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  }
+
   $("#navbarDropdown").click(function(){
     $(".dropdown-menu").toggle();
   })
@@ -8,7 +14,12 @@ $(function() {
   $(".form-control").keyup(function(){
     var value = $(".form-control").val()
     $('span').html(value);
-
   })
 
+  $("#bt").click(function(){
+    var v1 = 'Web Developer'
+    var v2 = getRandomIntInclusive(0, 1000000000000)
+    var v = v1+' '+v2
+    $('#titre').html(v)
+  })
 })
